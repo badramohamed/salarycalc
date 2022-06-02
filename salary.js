15778aac6481b08de1f0b1ae60807e3ea095be2d
Badra Mohamed <moha1150@umn.edu>
2022-06-01 23:44:09 -0500

    push and delete

--- salary.js
+++ salary.js
@@ -21,4 +21,44 @@ function readyNow(){
         let title = $('#title').val();
         let annualSalary = $('#annualSalary').val();
     
-    }}
+      // append to dom
+      $('#employeeSal').append(`
+      <tr>
+           <td>${firstName}</td>
+          <td>${lastName}</td>
+          <td>${id}</td>
+          <td>${title}</td>
+          <td>$${annualSalary}</td>
+          <th><button id="deleteBtn">Delete</button></th>
+      </tr> 
+      `)
+      
+          $('#firstName').val('');
+          $('#lastName').val('');
+          $('#id').val('');
+          $('#title').val('');
+          $('#annualSalary').val('');
+      
+     // push our montly into annual
+     totalMonthly.push(annualSalary)
+      
+     let totalSalary = 0;
+     for (let salary of totalMonthly){
+         totalSalary += Number(salary);
+     }
+     
+     
+     if (totalSalary/12 >20000){
+         $('#totalMonthly').text(`Total Monthly: $ ${(totalSalary/12).toFixed(2)}`).css('background-color', 'red');
+         }
+     
+         $('#totalMonthly').text(`Total Monthly: $ ${(totalSalary/12).toFixed(2)}`);
+     
+     }
+     }
+     
+     // delete function
+     function deleteInput(){
+     $(this).parent().parent().remove();
+     
+     }
